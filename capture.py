19f6b10c2ace039a66a6b0a3fac7840170d2eccd
Torin Anderson <148600795+torin-anderson@users.noreply.github.com>
2024-09-16 10:56:57 -0400

    Update capture.py to duration 5

--- capture.py
+++ capture.py
@@ -4,7 +4,7 @@ from video_screenshot import Keyframe
 kf = Keyframe()
 
 # change the file here for each set
-df = pd.read_csv("./get_keyframes/duration_set_4.csv")
+df = pd.read_csv("./get_keyframes/duration_set_5.csv")
 
 video_ids = df['videoId'].tolist()
 video_ids = [id.split(':')[1] for id in video_ids]
